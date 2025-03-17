6bbb3c642ac77ee28a656958b2013db00aaa81bf
Akshansh <akshanshsingh00@gmail.com>
2025-03-17 14:06:02 +0530

    Removed unused imports to solve preview deployment issue

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { redirect } from "next/navigation";
-
-import { options } from "../api/auth/[...nextauth]/options";
-import { getServerSession } from "next-auth/next";
 import { ArrowUpLeft, Construction } from "lucide-react";
 import Link from "next/link";
 const Leaderboard = async () => {
-  // using server session to get the user's session
-  // const session = await getServerSession(options);
-  // if (!session) {
-  //   redirect("/");
-  // }
-
   return (
     <div className="min-h-[calc(100vh-84px)] bg-primary p-8 flex flex-col items-center justify-center gap-24">
       <div className="flex flex-col items-center justify-center space-y-4">
